de2c00f33f86ddfa1d3ef2289cf9eb093a83822b
Alexis Guinebretiere <alexis.guineb@gmail.com>
2017-05-16 14:56:35 +0200

    exercice 1 partiellement terminé

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(name: "homework", done:"no")
+Task.create(name: "flashcards", done:"yes")
